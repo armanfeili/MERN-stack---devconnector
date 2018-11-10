4dac61454d5475b569b99fa197cf6cb07164753c
Arman Feili <armanfc.enter@yahoo.com>
2018-11-10 18:39:25 +0330

    post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Post model
 const Post = require('../../models/Post');
+// Validation
+const validatePostInput = require('../../validation/post');
 
 router.get('/test', (req, res) => {
   res.json({ msg: 'Posts works' });
@@ -13,10 +16,14 @@ router.get('/test', (req, res) => {
 // @desc    create post
 // @access  Private
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const {errors, isValid} = validatePostInput(req.body); // we pass all the req.body into validation file
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const newPost = new Post({
     text: req.body.text,
     name: req.body.name,
-    avatar: req.body.name,
+    avatar: req.body.avatar, // avatar will come from react application.
     id: req.user.id
   });
 
--- validation/post.js
+++ validation/post.js
@@ -1,13 +1,17 @@
 const validator = require('validator'); // validate has a lot of built-in functions like isEmail(),isLength,...
 const isEmpty = require('./is-empty'); // instead of installing lodash and use isEmpty() of that, we created that function.
 
-module.exports = function validateRegisterInput (data) { // it's just a function that when it called, it takes some data and make sure that it's as the way as we want
+module.exports = function validatePostInput (data) { // it's just a function that when it called, it takes some data and make sure that it's as the way as we want
   let errors = {}; // we create an empty errors object, so if there was an error, it gets fill
 
   // the required things
   data.text = !isEmpty(data.text) ? data.text : ''; // the isEmpty() method used here is the method we own created
   // here only text is required and stuff like name,avatar,date will come programmatically
-  if (!validator.isEmail(data.text)) {
+
+  if (!validator.isLength(data.text, {min: 4, max: 300})) {
+    errors.text = 'text must be between 4 and 300 characters';
+  }
+  if (validator.isEmpty(data.text)) {
     errors.text = 'Text feild is required!';
   }
 
