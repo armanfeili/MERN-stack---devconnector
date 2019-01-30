3e6285c3cf0afa489a3db23523e26dbe4fade687
Arman Feili <armanfc.enter@yahoo.com>
2019-01-30 19:53:15 +0330

    change10

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -25,8 +25,8 @@ class Landing extends Component {
                 <h1 className="display-3 mb-4">Developer Connector</h1>
                 <p className="lead">
                   {" "}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  Create a developer profile, share posts and get help from
+                  other developers
                 </p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">
