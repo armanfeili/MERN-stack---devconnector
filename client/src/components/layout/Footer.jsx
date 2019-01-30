4c0e0a683af6c58f149d4b1a61ebfe5e427c5a83
Arman Feili <armanfc.enter@yahoo.com>
2019-01-30 17:30:03 +0330

    changed7

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <footer className="bg-dark text-white mt-5  text-center footer">
       Copyright &copy; {new Date().getFullYear()} DevConnector
     </footer>
   );
